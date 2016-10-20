014c221e4ab99f017bdd42408a239ee82fab9538
xkl <xkl@beidou.com>
2016-10-20 09:32:05 +0800

    set

---
+++ iMessageAppOfXKL/MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by huiren on 16/10/20.
+//  Copyright © 2016年 xkl. All rights reserved.
+//
+
+#import <Messages/Messages.h>
+
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
